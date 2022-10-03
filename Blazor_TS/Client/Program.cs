f1d331b75e0a5728946ae1f7e1949e11a62a578c
Herbert Sales Santos <herbert.santos@clevertech.biz>
2022-10-03 14:45:38 -0300

    assestment upload to git

---
+++ Blazor_TS/Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazor_TS.Client;
+using Havit.Blazor.Components.Web;
+using Havit.Blazor.Components.Web.Bootstrap;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHxServices();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
+
---
+++ Blazor_TS/Shared/Country.cs
@@ -0,0 +1,30 @@
+﻿namespace Blazor_TS.Shared;
+using System.Text.Json.Serialization;
+
+
+public class Country
+{
+    [JsonPropertyName("post code")]
+    public string? PostCode { get; set; }
+    [JsonPropertyName("country")]
+    public string? CountryName { get; set; }
+    [JsonPropertyName("country abbreviation")]
+    public string? CountryAbbreviation { get; set; }
+    [JsonPropertyName("places")]
+    public Place[]? Places { get; set; }
+}
+
+public class Place
+{
+    [JsonPropertyName("place name")]
+    public string? PlaceName { get; set; }
+    [JsonPropertyName("longitude")]
+    public string? Longitude { get; set; }
+    [JsonPropertyName("latitude")]
+    public string? Latitude { get; set; }
+    [JsonPropertyName("state")]
+    public string? State { get; set; }
+    [JsonPropertyName("state abbreviation")]
+    public string? StateAbbreviation { get; set; }
+
+}
